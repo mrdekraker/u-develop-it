23ba58b019f28db203145aa4359f254f44103fbd
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-10-25 16:24:35 -0500

    edited server.js

--- db/server.js
+++ db/server.js
@@ -8,6 +8,45 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: `localhost`,
+    // Your MySQL username,
+    user: `root`,
+    // Your MySQL password
+    password: `River331!`,
+    database: `election`,
+  },
+  console.log(`Connected to the election database.`)
+);
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
